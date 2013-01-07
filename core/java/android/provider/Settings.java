df044bf837f3382895dc318d212386a5e47415e5
Danesh M <daneshm90@gmail.com>
2013-01-07 12:08:00 -0600

    AudioService: launch default music player on headset connect [1/2]
    Change-Id: Ibbb0607a56ab4d9246c14632eb4199558866854a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5336,6 +5336,12 @@ public final class Settings {
         public static final String SLIM_RECENTS_ICON_PACK = "slim_recents_icon_pack";
 
         /**
+         * Whether to launch default music player when headset plugged in
+         * @hide
+         */
+        public static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
