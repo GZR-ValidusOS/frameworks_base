b3c7efab3987c0979f7bb37030828d97ec59c157
yongga <yongga@codeaurora.org>
2016-01-14 17:46:52 +0800

    SystemUI:will display incorrect SIM pin code
    It will display incorrect SIM pin code even though
    Input correct PIN code after unlock the first SIM PIN
    
    Condition of judgment for dispaly message has error.
    Add condition of judgment for dispaly message.
    
    Change-Id: I7efb88617aa43ca653ffe0518d01a9007828cc9a
    CRs-Fixed: 960325

--- packages/Keyguard/src/com/android/keyguard/KeyguardSimPinView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardSimPinView.java
@@ -52,6 +52,7 @@ public class KeyguardSimPinView extends KeyguardPinBasedInputView {
     private CheckSimPin mCheckSimPinThread;
     private boolean mShowDefaultMessage = true;
     private int mRemainingAttempts = -1;
+    private int mResult = PhoneConstants.PIN_PASSWORD_INCORRECT;
     private AlertDialog mRemainingAttemptsDialog;
     private int mSubId;
     private int mSlotId;
@@ -264,6 +265,7 @@ public class KeyguardSimPinView extends KeyguardPinBasedInputView {
                         @Override
                         public void run() {
                             mRemainingAttempts = attemptsRemaining;
+                            mResult = result;
                             if (mSimUnlockProgressDialog != null) {
                                 mSimUnlockProgressDialog.hide();
                             }
@@ -272,6 +274,7 @@ public class KeyguardSimPinView extends KeyguardPinBasedInputView {
                             if (result == PhoneConstants.PIN_RESULT_SUCCESS) {
                                 KeyguardUpdateMonitor.getInstance(getContext())
                                         .reportSimUnlocked(mSubId);
+                                mResult = PhoneConstants.PIN_PASSWORD_INCORRECT;
                                 mRemainingAttempts = -1;
                                 mShowDefaultMessage = true;
                                 if (mCallback != null) {
@@ -326,8 +329,10 @@ public class KeyguardSimPinView extends KeyguardPinBasedInputView {
             return;
         }
         if (mRemainingAttempts >= 0) {
-            mSecurityMessageDisplay.setMessage(getPinPasswordErrorMessage(
-                    mRemainingAttempts, true), true);
+            if (mResult != PhoneConstants.PIN_RESULT_SUCCESS)
+                mSecurityMessageDisplay.setMessage(
+                        getPinPasswordErrorMessage(mRemainingAttempts, true),
+                        true);
             return;
         }
 
@@ -355,6 +360,7 @@ public class KeyguardSimPinView extends KeyguardPinBasedInputView {
                         " attemptsRemaining=" + attemptsRemaining);
                 if (attemptsRemaining >= 0) {
                     mRemainingAttempts = attemptsRemaining;
+                    mResult = result;
                     mSecurityMessageDisplay.setMessage(
                             getPinPasswordErrorMessage(attemptsRemaining, true), true);
                 }
