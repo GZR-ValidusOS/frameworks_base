3eb794beac8eaba51066d511d8d81e39c847cd98
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-11-25 09:41:29 +0100

    Bring back portrait/landscape QS mod [1/2]
    This is just adding back part of the code that we had before
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/faa0970f4c82d4a6c0b2e925cb09f626b4baac86
    
    https://i.imgur.com/pXsr3AH.png
    
    https://i.imgur.com/5ked5gQ.png
    
    https://i.imgur.com/9YCVYty.png
    
    Change-Id: Ibe7c035c2a101f8a2300d7bb2426c1a1659b85ce

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -473,6 +473,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
            resolver.registerContentObserver(Settings.System.getUriFor(
                   Settings.System.QS_LAYOUT_COLUMNS),
                   false, this, UserHandle.USER_ALL);
+           resolver.registerContentObserver(Settings.System.getUriFor(
+                  Settings.System.QS_ROWS_PORTRAIT),
+                  false, this, UserHandle.USER_ALL);
+           resolver.registerContentObserver(Settings.System.getUriFor(
+                  Settings.System.QS_ROWS_LANDSCAPE),
+                  false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_VALIDUS_LOGO),
                     false, this, UserHandle.USER_ALL);
@@ -489,6 +495,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         public void onChange(boolean selfChange, Uri uri) {
             super.onChange(selfChange, uri);
             if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.QS_ROWS_PORTRAIT))
+                    || uri.equals(Settings.System.getUriFor(
+                    Settings.System.QS_ROWS_LANDSCAPE))) {
+                    updateResources();
+            } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_SHOW_CARRIER))) {
                     updateSettings();
                     updateCarrier();
