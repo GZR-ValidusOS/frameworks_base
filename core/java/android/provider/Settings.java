be9e2a5d565d4b352254345fd14b1dab193e2a54
theringmaster <gzroms@hotmail.com>
2016-11-26 14:45:54 -0500

    Remove duplicate FLASHLIGHT_NOTIFICATION

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3834,12 +3834,6 @@ public final class Settings {
         public static final String POWERMENU_TORCH = "powermenu_torch";
 
         /**
-         * Enable flashlight notification
-         * @hide
-         */
-        public static final String FLASHLIGHT_NOTIFICATION = "flashlight_notification";
-
-        /**
          * Whether or not volume button music controls should be enabled to seek media tracks
          * @hide
          */
