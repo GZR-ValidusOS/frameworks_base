54c8bf7df0ebf82f4892c87600c1f449ecb64219
theringmaster <gzroms@hotmail.com>
2016-12-04 18:07:39 -0500

    Add isWifionly to ValidusUtils
    and repair dupe settings

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4111,21 +4111,6 @@ public final class Settings {
                 "volume_keys_control_media_stream";
 
         /**
-         * Whether or not volume button music controls should be enabled to seek media tracks
-         * @hide
-         */
-        public static final String VOLUME_ROCKER_MUSIC_CONTROLS = "volume_rocker_music_controls";
-
-        /**
-         * Swap volume buttons when the screen is rotated
-         * 0 - Disabled
-         * 1 - Enabled (screen is rotated by 90 or 180 degrees: phone, hybrid)
-         * 2 - Enabled (screen is rotated by 180 or 270 degrees: tablet)
-         * @hide
-         */
-        public static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
-
-        /**
          * Toast icon
          * @hide
          */
--- core/java/com/android/internal/util/validus/ValidusUtils.java
+++ core/java/com/android/internal/util/validus/ValidusUtils.java
@@ -26,6 +26,7 @@ import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
 import android.net.ConnectivityManager;
+import android.net.ConnectivityManager;
 import android.util.DisplayMetrics;
 import android.view.DisplayInfo;
 import android.view.WindowManager;
@@ -64,6 +65,12 @@ public class ValidusUtils {
             // Ignore
         }
         return false;
+    }
+
+    public static boolean isWifiOnly(Context context) {
+        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(
+                Context.CONNECTIVITY_SERVICE);
+        return (cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE) == false);
     }
 
     public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
