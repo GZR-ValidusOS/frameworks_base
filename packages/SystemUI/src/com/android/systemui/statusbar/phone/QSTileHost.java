8251d0840920256ff6f0e6e4d9d3e7aa280b6710
mickybart <mickybart@pygoscelis.org>
2016-12-29 13:19:26 +0800

    Squash: SystemUI: Tile: Lockscreen fixed for 7.0 by @Millosr
    SystemUI: Tile: Lockscreen: Fix lockscreen bypass
    Disable the Lockscreen tile when we are in the lockscreen and
    when this one is securized.
    
    That avoid to bypass the lockscreen (security issue)
    
    This replace state.visible unsupported since 7.0 and
    avoid any confusion for the end user because the tile is
    still visible but disabled.
    
    SystemUI: improve lockscreen tile behavior by romanbb
    * Stay disabled when disabled
    * No need to use Keyguard locks, use internal update monitor

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -59,6 +59,7 @@ import com.android.systemui.qs.tiles.ImeTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.LockscreenToggleTile;
 import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.NavigationBarTile;
 import com.android.systemui.qs.tiles.PieTile;
@@ -463,6 +464,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("heads_up")) return new HeadsUpTile(this);
         else if (tileSpec.equals("wolvesden")) return new WolvesDenTile(this);
         else if (tileSpec.equals("ambient")) return new AmbientLiftToWakeTile(this);
+        else if (tileSpec.equals("lockscreen")) return  new LockscreenToggleTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
