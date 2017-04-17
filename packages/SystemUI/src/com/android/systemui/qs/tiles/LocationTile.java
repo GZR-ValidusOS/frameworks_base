c7e39aa06781223cddf181ec20f644d200f3de66
maxwen <max.weninger@gmail.com>
2017-04-17 13:10:07 +0200

    base: SystemUI: qs: location tile quickbar press is just toggle
    like wifi and bluetooth - only expanded tile is advanced
    
    Change-Id: Idacec3afd7dbddf1af220af5a2465b8b9ff3c3a2

--- packages/SystemUI/src/com/android/systemui/qs/tiles/LocationTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/LocationTile.java
@@ -136,6 +136,12 @@ public class LocationTile extends QSTile<QSTile.State> {
     }
 
     @Override
+    protected void handleSecondaryClick() {
+        // just toggle here
+        mController.setLocationEnabled(!mController.isLocationEnabled());
+    }
+
+    @Override
     public Intent getLongClickIntent() {
         return null;
     }
